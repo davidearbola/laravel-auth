c946f66cf45d6ad1e595ce162872faa882dd08cb
davidearbola <davidearbola@gmail.com>
2024-07-18 14:00:59 +0200

    aggiunto Controller per project api

---
+++ app/Http/Controllers/Api/ProjectController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Project;
+use Illuminate\Http\Request;
+
+class ProjectController extends Controller
+{
+    public function index()
+    {
+        return response()->json([
+            'success' => true,
+            'projects' => Project::with(['type', 'languages'])->orderByDesc('id')->paginate()
+        ]);
+    }
+}
